698ba635fca0f8cb518c4c633f5a536e35795520
Quinn Vaughn <qvaughn3@gmail.com>
2023-11-24 15:34:33 -0800

    Added reject unauthorized.

--- src/lib/queue/connection.ts
+++ src/lib/queue/connection.ts
@@ -6,6 +6,8 @@ import IORedis from "ioredis"
 export const connection = new IORedis(config.REDIS_URL, {
 	maxRetriesPerRequest: null,
 	enableAutoPipelining: true,
-	tls: {},
+	tls: {
+		rejectUnauthorized: process.env.NODE_ENV !== "production",
+	},
 	commandTimeout: 10000,
 })
