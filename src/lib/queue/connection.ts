fceadc86ddf3b0b3522312068eb3010265e73da2
Quinn Vaughn <qvaughn3@gmail.com>
2023-11-24 19:23:49 -0800

    Trying to remove tls.

--- src/lib/queue/connection.ts
+++ src/lib/queue/connection.ts
@@ -6,8 +6,5 @@ import IORedis from "ioredis"
 export const connection = new IORedis(config.REDIS_URL, {
 	maxRetriesPerRequest: null,
 	enableAutoPipelining: true,
-	tls: process.env.NODE_ENV !== "production" ? undefined :{
-		rejectUnauthorized: false,
-	},
 	commandTimeout: 10000,
 })
